e35fac93a93042ac24eb2b9788e3f616b7080d17
Israel Mendes <israelnicolas940@gmail.com>
2026-01-29 23:45:55 -0300

    Initial commit

---
+++ 02-getting-started/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0b6c6ebed2801a5cb"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "terraform-example"
+  }
+}
