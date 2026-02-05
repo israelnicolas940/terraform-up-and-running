3ca6d1b0900fb2528991c1a615a2887744d36b15
Israel Mendes <israelnicolas940@gmail.com>
2026-02-05 00:06:03 -0300

    Create basic infrastructure with load balancer

--- 02-getting-started/main.tf
+++ 02-getting-started/main.tf
@@ -2,10 +2,175 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0b6c6ebed2801a5cb"
-  instance_type = "t3.micro"
+variable "server_port" {
+  description = "Server port"
+  type        = number
+  default     = 8080
+}
+
+variable "lb_port" {
+  description = "Load Balancer port"
+  type        = number
+  default     = 80
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port       = var.server_port
+    to_port         = var.server_port
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_launch_template" "example" {
+  name_prefix            = "example-lt-"
+  image_id               = "ami-0c398cb65a93047f2"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = base64encode(<<-EOF
+  #!/bin/bash
+  echo "Hello, World" > index.html
+  nohup busybox httpd -f -p ${var.server_port} &
+  EOF
+  )
+
   tags = {
     Name = "terraform-example"
   }
+}
+
+resource "aws_autoscaling_group" "servers" {
+  launch_template {
+    id      = aws_launch_template.example.id
+    version = "$Latest"
+  }
+  vpc_zone_identifier = data.aws_subnets.default.ids
+
+  target_group_arns = [aws_lb_target_group.servers.arn]
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+
+}
+
+# Security Group
+
+resource "aws_security_group" "alb" {
+  name   = "terraform-example-alb"
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all" {
+  security_group_id = aws_security_group.alb.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.alb.id
+  ip_protocol       = "tcp"
+  from_port         = var.lb_port
+  to_port           = var.lb_port
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+# End of Security Group
+
+resource "aws_lb" "public" {
+  name               = "example-dev-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = data.aws_subnets.default.ids
+
+  # enable_deletion_protection = true
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+resource "aws_lb_target_group" "servers" {
+  name_prefix = "ex-tg-"
+
+  port     = var.server_port
+  protocol = "HTTP"
+
+  vpc_id = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "api_http" {
+  load_balancer_arn = aws_lb.public.arn
+  port              = var.lb_port
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "forward_http" {
+  listener_arn = aws_lb_listener.api_http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.servers.arn
+  }
+}
+
+output "alb_dns_name" {
+  value       = aws_lb.public.dns_name
+  description = "The domain name of the load balancer"
 }
